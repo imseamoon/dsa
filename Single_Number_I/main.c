3268ea9358326efc0df09d4ff1ac410ed7b1d700
Seamoon Pandey <hackedasocial@gmail.com>
2024-05-31 08:39:11 +0545

    add solution to leetcode question single number - using bit manipulation

---
+++ Single_Number_I/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int singleNumber(int *nums, int numsSize)
+{
+    int ans = 0;
+    int xorSum = 0;
+
+    for (int i = 0; i < numsSize; i++)
+    {
+        xorSum ^= nums[i];
+    }
+
+    return xorSum;
+}
+
+int main(int argv, char *argc[])
+{
+
+    int array[] = {1, 1, 2, 2, 3, 3, 4};
+
+    int ans = singleNumber(array, sizeof(array) / sizeof(array[0]));
+    printf("%d", ans);
+    return 0;
+}
